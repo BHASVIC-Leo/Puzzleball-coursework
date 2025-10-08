f985ac4ac0b287264cbb9e6f9c9f2948d1089208
BHASVIC-Leo <leo.fidler24@students.bhasvic.ac.uk>
2025-10-08 11:46:24 +0100

    Ball

--- Puzzle ball/Ball.java
+++ Puzzle ball/Ball.java
@@ -12,6 +12,8 @@ public class Ball extends Actor
      * Act - do whatever the Ball wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    double vVel=0;
+    double hVel=0;
     public Ball(){
         getImage().scale(getImage().getWidth()/3, getImage().getHeight()/3);
     }
@@ -20,6 +22,35 @@ public class Ball extends Actor
         MyWorld world = (MyWorld)getWorld();
         pSlider pSlider = world.getpSlider();
         int power = pSlider.getPower();
-        move(power);
+        resolvehVel();
+        resolvevVel();
+        setLocation(getX()+(int)hVel,getY()-(int)vVel);
+    }
+    public void resolvehVel(){
+        MyWorld world = (MyWorld)getWorld();
+        pSlider pSlider = world.getpSlider();
+        aSlider aSlider = world.getaSlider();
+        int angle = aSlider.getAngle();
+        int power = pSlider.getPower();
+        if(getRotation()>=0){
+            hVel=power*Math.cos(angle);
+        }
+        else if(getRotation()<0){
+            hVel=power*Math.sin(-angle);
+        }
+    }
+    public void resolvevVel(){
+        MyWorld world = (MyWorld)getWorld();
+        aSlider aSlider = world.getaSlider();
+        int angle = aSlider.getAngle();
+        pSlider pSlider = world.getpSlider();
+        int power = pSlider.getPower();
+        if(getRotation()>=0){
+            vVel=power*Math.sin(angle);
+        }
+        else if(getRotation()<0){
+            vVel=power*Math.cos(-angle);
+        }
+        vVel=vVel-(9.81/60);
     }
 }
